07c6d061db3ae28da91903557fd1db8cd2dcebba
Uday1045 <tomaruday0412@gmail.com>
2025-08-31 19:46:20 +0530

    port

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import matchRoutes from "./routes/matches.js";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
